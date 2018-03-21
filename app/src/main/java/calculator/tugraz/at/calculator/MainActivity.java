f3f9d4ee848f5f2027b69a52a80871851a299c1b
Bernhard Danecker <bernhard.danecker@gmail.com>
2018-03-21 09:08:28 +0100

    Implemented inputField and digit buttons function

--- app/src/main/java/calculator/tugraz/at/calculator/MainActivity.java
+++ app/src/main/java/calculator/tugraz/at/calculator/MainActivity.java
@@ -1,13 +1,40 @@
 package calculator.tugraz.at.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView inputText;
+    private List<Button> numberButtons​ = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputText = findViewById(R.id.txt_input);
+
+        for (int i = 0; i <= 9; i++) {
+            int buttonId = getResources().getIdentifier("button" + i, "id", R.class.getPackage().getName());
+            Button button = findViewById(buttonId);
+            button.setOnClickListener(this);
+
+            numberButtons​.add(button);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (numberButtons​.contains(v)) {
+            Button button = (Button) v;
+            inputText.setText(inputText.getText().toString() + button.getText());
+        }
     }
 }
