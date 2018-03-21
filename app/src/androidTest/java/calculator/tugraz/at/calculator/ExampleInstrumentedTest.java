00f4630481dff4e94168f39a262c8940fd88c4e4
Bernhard Danecker <bernhard.danecker@gmail.com>
2018-03-21 08:28:13 +0100

    Initial commit

---
+++ app/src/androidTest/java/calculator/tugraz/at/calculator/ExampleInstrumentedTest.java
@@ -0,0 +1,48 @@
+package calculator.tugraz.at.calculator;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("calculator.tugraz.at.calculator", appContext.getPackageName());
+    }
+
+    @Test
+    public void testButtons() {
+        for (int i = 0; i <= 9; i++) {
+            onView(withText(Integer.toString(i))).perform(click());
+        }
+        onView(withText("+")).perform(click());
+        onView(withText("-")).perform(click());
+        onView(withText("*")).perform(click());
+        onView(withText("/")).perform(click());
+        onView(withText("=")).perform(click());
+        onView(withText("C")).perform(click());
+    }
+}
